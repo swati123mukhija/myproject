91b0c9b008defc630f875c27faa4eed8c1681f8f
swati.makhija <swati.makhija@yatra.com>
2020-07-17 14:11:48 +0530

    vuejs card project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>Fetch data with API</h1>
+    <Cards/>
+  </div>
+</template>
+<script>
+import Cards from './components/card'
+import Home from './components/home'
+export default {
+  name: 'App',
+  components: {
+    Cards
+  }
+};
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+ -webkit-font-smoothing: antialiased;
+ -moz-osx-font-smoothing: grayscale;
+ text-align: center;
+ color: #2c3e50;
+ margin-top: 60px;
+}
+@import'~bootstrap/dist/css/bootstrap.css'
+</style>
---
+++ src/components/card.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Card API</h1>
+    
+  <div style="display: flex;
+    flex-wrap: wrap;
+    justify-content: center;">
+<div v-for = "item in list" style="margin-right: 10px;">
+    <b-card no-body>{{item.id}}
+        <b-card-group deck>
+           <b-card img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top style="max-width: 240px;" class="mb-2">
+             <b-card-title style="display: inline;">{{item.title}}</b-card-title>
+          </b-card>
+        </b-card-group>
+    </b-card>
+  
+</div>
+  </div>
+</div>
+</template>
+
+<script>
+import Vue from 'vue';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+Vue.use(VueAxios, axios)
+export default{
+  name: "card",
+  data(){
+    return{
+      list:[]
+    }
+  },
+  mounted(){
+    Vue.axios.get('https://jsonplaceholder.typicode.com/todos/').then(resp=>{
+      console.log(resp.data);
+      this.list = resp.data;
+    })
+    .catch(function(error){
+      console.log("Error", error);
+    })
+  }
+}
+  </script>
