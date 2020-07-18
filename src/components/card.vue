1d1ee7a95a8c09b8576ec29e1a800c9cdeb83845
swati.makhija <swati.makhija@yatra.com>
2020-07-18 15:28:43 +0530

    data store card

--- src/components/card.vue
+++ src/components/card.vue
@@ -4,7 +4,7 @@
     <div style="display: flex;
     flex-wrap: wrap;
     justify-content: center;">
-<div v-for = "item in list" style="margin-right: 10px;">
+<div v-for = "item in storedata" style="margin-right: 10px;">
     <b-card no-body>{{item.id}}
         <b-card-group deck>
            <b-card img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top style="max-width: 240px;" class="mb-2">
@@ -22,18 +22,19 @@
 import Vue from 'vue';
 import VueAxios from 'vue-axios';
 import axios from 'axios';
+import Datacard from '../store';
 Vue.use(VueAxios, axios)
 export default{
   name: "card",
   data(){
     return{
-      list:[]
+      storedata: Datacard.data
     }
   },
   mounted(){
     Vue.axios.get('https://jsonplaceholder.typicode.com/todos/').then(resp=>{
       console.log(resp.data);
-      this.list = resp.data;
+      this.storedata = resp.data;
     })
     .catch(function(error){
       console.log("Error", error);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import store from './store';
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -9,7 +10,7 @@ Vue.use(BootstrapVue);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   components: { App },
   template: '<App/>'
 })
-src
